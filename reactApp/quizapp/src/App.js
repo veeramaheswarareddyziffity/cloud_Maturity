9b83670b9f92193ca43e2cab61a230d61f177a90
John Doe <john.doe@example.com>
2024-01-30 17:35:14 +0530

    react app

---
+++ reactApp/quizapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Questions from './Components/Questions';
+
+function App() {
+  return (
+    <div className="App">
+  
+      <Questions />
+    </div>
+  );
+}
+
+export default App;
